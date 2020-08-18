9ada953e3a4f5d9fb6ebefd0e9cbdcd55a10db33
Juan Pablo Acosta <vporx100@gmail.com>
2020-08-18 09:49:40 -0500

    Finish carrusel styles

--- app/javascript/components/Videogames/Carrusel.js
+++ app/javascript/components/Videogames/Carrusel.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Carrusel = (props) => {
     return(
         <div className = "carrusel">
-            <ul className = "imag">{props.images}</ul>
+            <ul>{props.images}</ul>
         </div>
     )
 }
